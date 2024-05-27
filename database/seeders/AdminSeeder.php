6c9bc7b295dfa87f4a2326810f96d28b3934c59f
kurt john <kredoteacher.kurt@gmail.com>
2024-05-27 16:29:07 +0800

    added seeders

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    private $user;
+    public function __construct(User $user){
+        $this->user = $user;
+    }
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $this->user->name = 'Administrator';
+        $this->user->email = 'admin@email.com';
+        $this->user->password = Hash::make('admin12345');
+        $this->user->role_id = 1;
+        $this->user->save();
+    }
+}
